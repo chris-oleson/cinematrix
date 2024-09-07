da3ab16c9f21cc2b8b2370645bc44e2fe5de004a
Chris Oleson <crolesonjr@gmail.com>
2024-09-07 17:52:27 -0600

    Fixing styling issues on other devices

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="text-center pa-8 container">
-        <div class="d-flex justify-center">
-            <img src="/src/assets/logo.png" height="70px">
-            <h1 class="text-h2 shadow">Cinematrix</h1>
+    <div class="d-flex align-center justify-center text-center pa-8" style="height:80%">
+        <div style="width:700px">
+            <div class="d-flex justify-center align-center">
+                <img src="/src/assets/logo.png" height="70px">
+                <h1 class="text-h2 shadow">Cinematrix</h1>
+            </div>
+            <h2 class="font-weight-light my-8 shadow">All of film at your fingertips</h2>
+            <v-text-field v-model="query" variant="solo" theme="light" hide-details label="Search movies" append-inner-icon="mdi-magnify" @keyup.enter="search()" @click:append-inner="search" style="box-shadow: 3px 3px 8px black;"></v-text-field>
         </div>
-        <h2 class="font-weight-light my-8 shadow">All of film at your fingertips</h2>
-        <v-text-field v-model="query" variant="solo" theme="light" hide-details label="Search movies" append-inner-icon="mdi-magnify" @keyup.enter="search()" @click:append-inner="search" class="mx-auto" max-width="800px" style="box-shadow: 3px 3px 8px black;"></v-text-field>
     </div>
 </template>
 
@@ -20,11 +22,3 @@ function search() {
     router.push(`/search?q=${query.value}&p=1`)
 }
 </script>
-
-<style scoped>
-.container {
-    height: 80%;
-    align-content: center;
-    -webkit-align-content: center;
-}
-</style>
--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mx-4 my-16 d-flex flex-wrap justify-center">
         <img :src="movie.Poster" class="ma-4" style="box-shadow: 3px 3px 8px black;">
-        <div class="ma-4 shadow" style="width: 700px;">
+        <div class="ma-4 shadow">
             <div class="d-flex">
                 <h1>{{ movie.Title }} ({{ movie.Year }})</h1>
                 <v-tooltip :text="getTooltip" content-class="bg-transparent" offset="0">
@@ -12,7 +12,8 @@
                     </template>
                 </v-tooltip>
             </div>
-            <div class="my-4">{{ movie.Plot }}</div>
+            
+            <div class="my-4" style="max-width:700px">{{ movie.Plot }}</div>
 
             <div class="d-flex">
                 <div class="font-weight-bold mr-6">
